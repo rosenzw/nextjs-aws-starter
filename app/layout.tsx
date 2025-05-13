4dd3acd412c764751cb6bf93a5047da6a0ebd8c3
rosenzw <jesseswig@gmail.com>
2025-05-13 06:35:22 -0700

    Removing ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import './globals.css'
-import Script from 'next/script'
 import { Analytics } from "@vercel/analytics/react"
-import AdBanner from './components/AdBanner'
 
 export const metadata = {
   title: 'CS467 App Template',
@@ -15,13 +13,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9549547938638588"
-          crossOrigin="anonymous"></script>
-      </head>
       <body>
         {children}
-        <AdBanner />
         <Analytics />
       </body>
     </html>
