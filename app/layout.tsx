91b4cfab7d5640493c9140692766d3e5840679bf
rosenzw <jesseswig@gmail.com>
2025-05-12 21:50:09 -0700

    changing ad insertion approach

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 import './globals.css'
+import Script from 'next/script'
 
 import { Analytics } from "@vercel/analytics/react"
 
 export const metadata = {
-  title: 'Next.js',
-  description: 'Starter NextJS application that integrates with AWS S3 and DynamoDB.',
+  title: 'CS467 App Template',
+  description: 'A starter NextJS application that integrates with AWS S3 and DynamoDB',
 }
 
 export default function RootLayout({
@@ -14,6 +15,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9549547938638588"
+          crossOrigin="anonymous"></script>
+      </head>
       <body>
         {children}
         <Analytics />
